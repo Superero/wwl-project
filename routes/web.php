5b15032777c6136b168b23c30008fc0c032c4485
Superero <ahmed.bmd375@gmail.com>
2026-08-31 21:16:44 +0200

    modify dashboard roote

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,11 @@ Route::get('/', function () {
     return view('index');
 })->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboard', [ConsultationController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
