8e2a12d26ee6d53c2f6754a04b65f55ba3027a1c
Superero <ahmed.bmd375@gmail.com>
2026-09-02 17:06:40 +0200

    change dispatch to dispatchSync in consultation contrl

--- app/Http/Controllers/ConsultationRequestController.php
+++ app/Http/Controllers/ConsultationRequestController.php
@@ -12,7 +12,7 @@ class ConsultationRequestController extends Controller
     public function store(StoreConsultationRequest $request){
         $consultation = ConsultationRequest::create($request->validated());
         \Log::info('Dispatch SendToGoogleSheets pour la demande #' . $consultation->id);
-        SendToGoogleSheets::dispatch($consultation);
+        SendToGoogleSheets::dispatchSync($consultation);
 
         return back()->with('success', 'Votre demande a bien été envoyée. Un expert vous recontacte sous 24h.');
     }
